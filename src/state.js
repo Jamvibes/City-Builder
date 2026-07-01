7947f0ea5a787fb441b5dd98708e2ae461eb6741
Jamvibes <James_ray_li@yahoo.com.au>
2026-07-01 18:38:48 +1000

    Add game state module

---
+++ src/state.js
@@ -0,0 +1,41 @@
+import { tileTypes } from "./tiles.js";
+
+export const canvas = document.getElementById("gameCanvas");
+export const ctx = canvas.getContext("2d");
+
+export const hexSize = 40;
+export const centerX = canvas.width / 2;
+export const centerY = canvas.height / 2;
+
+export const directions = [
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1]
+];
+
+export const placedTiles = {
+  "0,0": {
+    q: 0,
+    r: 0,
+    type: "townCentre"
+  }
+};
+
+export const terrainTiles = {};
+
+export const resources = {
+  food: 0,
+  wood: 0,
+  stone: 0,
+  gold: tileTypes.townCentre.resources.gold,
+  population: 0
+};
+
+export const gameState = {
+  turnCount: 1,
+  currentOptions: [],
+  selectedTile: null
+};
