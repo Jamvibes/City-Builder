89d129b309e92791437f041945de71ea1d911efc
Jamvibes <James_ray_li@yahoo.com.au>
2026-07-01 18:56:57 +1000

    Use shared placement rules module

--- src/placement.js
+++ src/placement.js
@@ -1,41 +1,9 @@
-import { directions, gameState, placedTiles, resources } from "./state.js";
+import { gameState, placedTiles, resources } from "./state.js";
 import { tileTypes } from "./tiles.js";
 import { draw } from "./renderer.js";
 import { drawRandomOptions } from "./tileOptions.js";
 import { updateResources, updateTurns } from "./hud.js";
-
-export function getValidPlacementSpots() {
-  const spots = {};
-
-  for (const key in placedTiles) {
-    const tile = placedTiles[key];
-
-    for (const [dq, dr] of directions) {
-      const q = tile.q + dq;
-      const r = tile.r + dr;
-      const newKey = `${q},${r}`;
-
-      if (!placedTiles[newKey]) {
-        spots[newKey] = { q, r };
-      }
-    }
-  }
-
-  return Object.values(spots);
-}
-
-export function canPlaceTile(q, r) {
-  const key = `${q},${r}`;
-
-  if (placedTiles[key]) {
-    return false;
-  }
-
-  return directions.some(([dq, dr]) => {
-    const neighbourKey = `${q + dq},${r + dr}`;
-    return placedTiles[neighbourKey];
-  });
-}
+import { canPlaceTile } from "./placementRules.js";
 
 export function placeTile(q, r, type) {
   if (!type || !canPlaceTile(q, r)) {
